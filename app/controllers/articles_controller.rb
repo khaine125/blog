9f2ba6764e758f1823e2510a93eeab9d5a3fac25
Bad Motherfucker <d.mackovic@gmail.com>
2020-06-22 01:21:25 +0200

    7. poglavlje refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,10 @@ class ArticlesController < ApplicationController
   def new
     @article = Article.new
   end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end
 
   def index
     @articles = Article.all
@@ -19,6 +23,23 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+  end
+  
+#  def update
+#    @article = Article.find(params[:id])
+#    
+#    if @article.update(article_params)
+#      redirec_to @article
+#    else
+#      render "edit"
+#    end
+#  end
+  
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    
+    redirect_to articles_path
   end
 
 private
